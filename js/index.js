d4628948df4ca0278dc9735f1f2da59a29e6516b
Park Jack <parker.jackson.hhs@gmail.com>
2021-03-26 11:03:13 -0400

    minor changes made

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let solarSystem = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Planet Nine"]
+
+function createPlanetCards(arg){for(i=0;i<arg.length;i++){ 
+document.getElementsByTagName("BODY")[0].innerHTML = i
+
+
+}}
+createPlanetCards(solarSystem);
