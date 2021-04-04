3c5e9aff085ba274cd452abcc5a5b0a0852e87a6
Parker J <parkerj@Parkers-iMac.local>
2021-04-04 13:56:12 -0400

    js function that dynamically writes planets on the page by a button push

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
-let solarSystem = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Planet Nine"]
 
-function createPlanetCards(arg){for(i=0;i<arg.length;i++){ 
-document.getElementsByTagName("BODY")[0].innerHTML = i
+ 
 
 
-}}
-createPlanetCards(solarSystem);
+
+
+
+
+    let solarSystem = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Planet Nine"]
+    console.log(`Blue rain ${solarSystem}`) 
+
+
+    let heart5 = document.getElementById("solarSystem1")
+    let planetCardsArea = document.createElement("DIV")
+    heart5.appendChild(planetCardsArea)
+
+    function createPlanetCards(x){
+        for (i = 0; i < x.length; i++) {
+            let planetCard = document.createElement("DIV")
+            planetCard.innerHTML = x[i]
+            planetCardsArea.appendChild(planetCard)
+            console.log(i)
+    
+    
+        }
+ 
+    }
+
+
+
+// window.onload = createPlanetCards(solarSystem);
